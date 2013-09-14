1d91da7e0b6fe742ebcb2d25286778f0a8b5d7a3
NCrashed <NCrashed@gmail.com>
2013-09-14 18:21:53 +0800

    Допилил диалог настроек и кнопку отправки сообщения.

--- src/powercom/Main.hs
+++ src/powercom/Main.hs
@@ -17,6 +17,11 @@ module Main (main) where
 
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Multiline.TextBuffer
+import Graphics.UI.Gtk.Windows.Dialog
+import System.Hardware.Serialport
+import Data.Functor
+import Control.Applicative
+import Control.Monad
 
 menuBarDescr
     = [ ("_Файл", [ ("_Открыть историю",    Nothing)
@@ -24,13 +29,14 @@ menuBarDescr
                   , ("_Выход",              Just mainQuit)
                   ]
         )
-      , ("Помощь",  [ ("_О программе", Nothing)
+      , ("_Помощь",  [ ("_О программе", Nothing)
                   ]
         )
       ]
 
-commandBarDescr
-    = [ ("_Настройки",    Nothing)
+commandBarDescr optionDialog
+    = [ ("_Настройки",    Just $ do widgetShowAll optionDialog 
+                                    return ())
       , ("_Открыть порт", Nothing)
       , ("_Закрыть порт", Nothing)
       , ("_Подключиться", Nothing)
@@ -80,24 +86,27 @@ createMessageArea =
        textView    <- textViewNew
        messageEdit <- entryNew
        boxPackStart box textView PackGrow 0
-       boxPackStart box messageEdit PackNatural 0
+       hbox <- hBoxNew False 5
+       sendBtn <- buttonNewWithLabel "Отправить"
+       boxPackStart hbox messageEdit PackGrow 0
+       boxPackStart hbox sendBtn PackNatural 0
+       boxPackStart box hbox PackNatural 0
        return box
 
-createMainWindowArea = 
+createMainWindowArea bar = 
     do box <- hBoxNew False 5
        messageArea <- createMessageArea
-       commandArea <- createCommandArea commandBarDescr
+       commandArea <- createCommandArea bar
        boxPackStart box messageArea PackGrow 0
        boxPackStart box commandArea PackNatural 0
        return box
 
-main =
-    do initGUI
-       window <- windowNew
+createMainWindow bar =
+    do window <- windowNew
 
        box <- vBoxNew False 5
        menuBar <- createMenuBar menuBarDescr
-       mainArea <- createMainWindowArea
+       mainArea <- createMainWindowArea bar
        boxPackStart box menuBar PackNatural 0
        boxPackStart box mainArea PackGrow 0
 
@@ -107,5 +116,81 @@ main =
                   , containerChild := box
                   ]
        onDestroy window mainQuit
-       widgetShowAll window
+       return window
+
+createOptionDialog =
+    do dialog <- dialogNew
+       windowSetModal dialog True
+       set dialog [windowTitle := "Настройки"]
+       box <- dialogGetUpper dialog
+
+       speedCombo <- createEnumCombo [("110",    CS110)
+                                     ,("300",    CS300)
+                                     ,("600",    CS600)
+                                     ,("1200",   CS1200)
+                                     ,("2400",   CS2400)
+                                     ,("4800",   CS4800)
+                                     ,("9600",   CS9600)
+                                     ,("19200",  CS19200)
+                                     ,("38400",  CS38400)
+                                     ,("57600",  CS57600)
+                                     ,("115200", CS115200)]
+
+       stopBitsCombo <- createEnumCombo [("One", One)
+                                        ,("Two", Two)]
+
+       parityCombo <- createEnumCombo [("Even", Even)
+                                      ,("Odd",  Odd)
+                                      ,("None", NoParity)]
+
+       dataBitsCombo <- createEnumCombo [("7", 7)
+                                        ,("8", 8)
+                                        ,("9", 9)]
+       portEntry <- entryNew
+       nickEntry <- entryNew
+
+       packTable box [("Имя порта:", toWidget portEntry)
+                     ,("Псевдоним:", toWidget nickEntry)
+                     ,("Скорость:",  toWidget speedCombo)
+                     ,("Стоп биты:", toWidget stopBitsCombo)
+                     ,("Бит четности:", toWidget parityCombo)
+                     ,("Биты данный", toWidget dataBitsCombo)]
+       
+       dialogAddButton dialog "OK" ResponseOk
+       dialogAddButton dialog "Cancel" ResponseCancel
+       dialog `on` response $ \respId -> do
+          case respId of 
+            ResponseOk -> putStrLn "Ok button pressed!"
+            ResponseCancel -> putStrLn "Cancel button pressed!"
+            ResponseDeleteEvent -> putStrLn "Close button pressed!"
+          widgetHideAll dialog
+       return dialog
+    where
+       packTable :: (BoxClass a, WidgetClass b) => a -> [(String, b)] -> IO ()
+       packTable parent descr =
+          do table <- tableNew (length descr) 2 True
+             foldM_ (packTableRow table) 0 descr
+             boxPackStart parent table PackGrow 0
+          where 
+            packTableRow table row (text, widget) =
+              do label <- labelNew $ Just text 
+                 tableAttachDefaults table label  0 1 row (row+1)
+                 tableAttachDefaults table widget 1 2 row (row+1)
+                 return $ row+1
+
+       createEnumCombo descr =
+          do combo <- comboBoxNew
+             store <- listStoreNew descr
+             cell <- cellRendererTextNew
+             cellLayoutPackStart combo cell True
+             cellLayoutSetAttributes combo cell store $ 
+                \(text, value) -> [cellText := text]
+             comboBoxSetModel combo $ Just store
+             return combo
+
+main =
+    do initGUI
+       optionDialog <- createOptionDialog
+       mainWindow <- createMainWindow (commandBarDescr optionDialog)
+       widgetShowAll mainWindow
        mainGUI
